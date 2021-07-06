961fedd68977ed079d0bc070331eebf5df07a063
Justin-Ding-Siou <69789527+Justin-Ding-Siou@users.noreply.github.com>
2021-07-06 17:57:25 +0800

    加入專案檔案。

---
+++ vagetableAPI/Controllers/FoodController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using vagetableAPI.Models;
+namespace vagetableAPI.Controllers
+{
+    public class FoodController : ApiController
+    {
+        // GET: api/Food
+        dbVagetableBasketEntities db = new dbVagetableBasketEntities();
+        // GET: api/Food
+        public List<Food> Get()
+        {
+            var foods = db.Food;
+            return foods.ToList();
+        }
+
+        // GET: api/Food/5
+        public Food Get(int fridge_id)
+        {
+            var foods = db.Food
+                .Where(m => m.fridge_id == fridge_id).FirstOrDefault();
+            return foods;
+        }
+
+        // POST: api/Food
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Food/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Food/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ vagetableAPI/Controllers/RecipeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using vagetableAPI.Models;
+
+namespace vagetableAPI.Controllers
+{
+    public class RecipeController : ApiController
+    {
+        dbVagetableBasketEntities db = new dbVagetableBasketEntities();
+        // GET: api/Recipe
+        public List<Recipe> Get()
+        {
+            var customers = db.Recipe;
+            return customers.ToList();
+        }
+
+        // GET: api/Recipe/5
+        public Recipe Get(int recipe_id)
+        {
+            var customer = db.Recipe
+                .Where(m => m.recipe_id == recipe_id).FirstOrDefault();
+            return customer;
+        }
+
+        // POST: api/Recipe
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Recipe/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Recipe/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
